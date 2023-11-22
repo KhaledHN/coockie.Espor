f26e0ad9ac0069816d92c27b255275a977af6690
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-11-22 20:34:07 +0300

    Making the form send email

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -1,12 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { useForm, ValidationError } from "@formspree/react";
+import React, { useState } from "react";
+import { useForm } from "@formspree/react";
 
 const ContactForm = () => {
   const [activePage, setActivePage] = useState(1);
   const [state, handleSubmit] = useForm("mjvqddzv");
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
-  }
+
+  // Your form data
+  const formData = {
+    name: "",
+    ad: "",
+    isim: "",
+    soyad: "",
+    lolnic: "",
+    discord: "",
+    ad2: "",
+    soyad2: "",
+    lolnic2: "",
+    discord2: "",
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    formData[name] = value;
+  };
 
   // Function to toggle between pages
   const togglePages = () => {
@@ -18,182 +34,201 @@ const ContactForm = () => {
     setActivePage((prevPage) => (prevPage === 1 ? 3 : prevPage - 1));
   };
 
+  // Submit handler
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    try {
+      await handleSubmit(e);
+
+      // Form submission was successful
+      if (activePage === 3) {
+        // If on the last page, display "Thanks for joining!" message
+        setActivePage(4);
+      } else {
+        // If not on the last page, move to the next page
+        togglePages();
+      }
+    } catch (error) {
+      // Handle errors
+      console.error(error);
+    }
+  };
+
   return (
-    <div className=" w-full absolute h-full mx-auto p-6 bg-transparent rounded-md shadow-md">
-      <div className=" max-w-[1400px] pt-28 w-full h-full mx-auto py-10 flex  justify-center items-center ">
-        <div className=" w-[400px] ">
+    <div className="w-full absolute h-full mx-auto p-6 bg-transparent rounded-md shadow-md">
+      <div className="max-w-[1400px] w-full h-full mx-auto py-10 flex justify-center items-center ">
+        <div className="w-[400px] ">
           <div className=" ">
-            <h2 className=" text-white text-2xl font-semibold mb-2">
-              <span className=" text-[#ad7a41] ">Cookie</span> E-spor'a Hoş
-              Geldin
+            <h2 className="text-white text-2xl font-semibold mb-2">
+              <span className="text-[#ad7a41]">Cookie</span> E-spor'a Hoş Geldin
             </h2>
-            <h2 className=" text-gray-300 text-left font-inter text-base font-medium  mb-6 ">
+            <h2 className="text-gray-300 text-left font-inter text-base font-medium  mb-6 ">
               Cookie'lerle dolu ligimize hoş geldin, oyunun kralı sen olmaya
               hazır mısın?
             </h2>
           </div>
-          {activePage === 1 && (
-            <form>
-              <div className="mb-4">
-                <label
-                  className="block text-gray-200 text-sm font-bold mb-2"
-                  htmlFor="name"
-                >
-                  Takım Adını Girin:
-                </label>
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="Takım Adı Giriniz"
-                  name="name"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  className="block text-gray-200 text-sm font-bold mb-2"
-                  htmlFor="message"
-                ></label>
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="Takım Kısaltması Giriniz"
-                  name="ad"
-                  required
-                />
-              </div>
-              <div className=" w-full  ">
-                <button
-                  className=" w-full bg-[#212529] text-white p-3 rounded-full shadow-lg shadow-gray-800 hover:bg-gray-900 duration-200 focus:outline-none focus:shadow-outline-blue"
-                  onClick={togglePages}
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-          )}
-
-          {activePage === 2 && (
-            <form>
-              <div className="mb-5">
-                <label
-                  className="block text-gray-200 text-md font-bold mb-4"
-                  htmlFor="massage"
-                >
-                  1. Oyuncu Bilgileri (Kaptan - Zorunlu)
-                </label>
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-lg shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="isim"
-                  name="isim"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="Soyadınız"
-                  name="soyad"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="League Of Legends Nickiniz"
-                  name="lolnic"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="Discord Nickiniz"
-                  name="Discord"
-                  required
-                />
-              </div>
-
-              <div className=" w-full  ">
-                <button
-                  className=" w-full bg-[#212529] text-white p-3 rounded-full shadow-md shadow-gray-800 hover:bg-gray-900 duration-200 focus:outline-none focus:shadow-outline-blue"
-                  onClick={togglePages}
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-          )}
-
-          {activePage === 3 && (
-            <form>
-              <div className="mb-5">
-                <label
-                  className="block text-gray-200 text-md font-bold mb-4"
-                  htmlFor="name"
-                >
-                  2. Oyuncu Bilgileri (Zorunlu)
-                </label>
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-lg shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="Adınız"
-                  name="ad2"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="Soyadınız"
-                  name="soyad2"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="League Of Legends Nickiniz"
-                  name="lolnic2"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <input
-                  className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600 "
-                  type="text"
-                  placeholder="Discord Nickiniz"
-                  name="discord2"
-                  required
-                />
-              </div>
-
-              <div className=" w-full  ">
+          {activePage === 4 ? (
+            <p className="text-white">Thanks for joining!</p>
+          ) : (
+            <form onSubmit={submitForm}>
+              {activePage === 1 && (
+                <div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-200 text-sm font-bold mb-2"
+                      htmlFor="name"
+                    >
+                      Takım Adını Girin:
+                    </label>
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="Takım Adı Giriniz"
+                      name="name"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-200 text-sm font-bold mb-2"
+                      htmlFor="ad"
+                    >
+                      Takım Kısaltması Giriniz:
+                    </label>
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="Takım Kısaltması Giriniz"
+                      name="ad"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                </div>
+              )}
+              {activePage === 2 && (
+                <div>
+                  <div className="mb-5">
+                    <label
+                      className="block text-gray-200 text-md font-bold mb-4"
+                      htmlFor="isim"
+                    >
+                      1. Oyuncu Bilgileri (Kaptan - Zorunlu)
+                    </label>
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-lg shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="isim"
+                      name="isim"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="Soyadınız"
+                      name="soyad"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="League Of Legends Nickiniz"
+                      name="lolnic"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="Discord Nickiniz"
+                      name="discord"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                </div>
+              )}
+              {activePage === 3 && (
+                <div>
+                  <div className="mb-5">
+                    <label
+                      className="block text-gray-200 text-md font-bold mb-4"
+                      htmlFor="ad2"
+                    >
+                      2. Oyuncu Bilgileri (Zorunlu)
+                    </label>
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-lg shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="Adınız"
+                      name="ad2"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="Soyadınız"
+                      name="soyad2"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="League Of Legends Nickiniz"
+                      name="lolnic2"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <input
+                      className="w-full p-2 pl-5 border border-gray-300 rounded-full shadow-md shadow-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-600"
+                      type="text"
+                      placeholder="Discord Nickiniz"
+                      name="discord2"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                </div>
+              )}
+              <div className="w-full ">
                 <button
                   type="submit"
                   disabled={state.submitting}
-                  className=" w-full bg-[#212529] text-white p-3 rounded-full shadow-md shadow-gray-800 hover:bg-gray-900 duration-200 focus:outline-none focus:shadow-outline-blue"
+                  className="w-full bg-[#212529] text-white p-3 rounded-full shadow-md shadow-gray-800 hover:bg-gray-900 duration-200 focus:outline-none focus:shadow-outline-blue"
                 >
                   Submit
                 </button>
               </div>
             </form>
           )}
-
-          <div className=" w-full pl-[150px] pt-24">
-            <button
-              className=" w-[40%] bg-[#212529] text-white p-3 rounded-full shadow-lg shadow-gray-800 hover:bg-gray-900 duration-200 focus:outline-none focus:shadow-outline-blue "
-              onClick={goBack}
-            >
-              Back
-            </button>
-          </div>
+          {activePage !== 1 && activePage !== 4 && (
+            <div className="w-full pl-[150px] pt-24">
+              <button
+                className="w-[40%] bg-[#212529] text-white p-3 rounded-full shadow-lg shadow-gray-800 hover:bg-gray-900 duration-200 focus:outline-none focus:shadow-outline-blue "
+                onClick={goBack}
+              >
+                Back
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,28 +3,26 @@ import React from "react";
 const Main = () => {
   return (
     <div className=" w-full min-h-screen flex items-center justify-center">
-      <div className=" pl-[485px] w-full ">
-        <div className=" w-[500px] ">
-          <div className="text-center">
-            <h1 className=" text-white text-4xl font-bold mb-4">
-              {" "}
-              <span className=" text-[#ad7a41] ">Cookie</span> x Kozmos
-            </h1>
-            <h2 className=" text-white text-3xl font-bold mb-4 ">
-              <span className=" text-[#D9A65D] ">League Of Legends </span>
-              Turnuvası
-            </h2>
-            <div className="flex justify-center pt-5 space-x-4">
-              <a
-                href="/ContactForm"
-                className="bg-black hover:scale-110 duration-150 hover:bg-gray-800 shadow-md shadow-gray-700 text-white font-bold py-2 px-4 rounded-full"
-              >
-                Kayit Ol
-              </a>
-              <button className="bg-transparent border border-transparent hover:border-gray-500 text-white font-bold py-2 px-4 rounded-full">
-                Kurallari Oku
-              </button>
-            </div>
+      <div className=" w-[500px] ">
+        <div className="text-center">
+          <h1 className=" text-white text-4xl font-bold mb-4">
+            {" "}
+            <span className=" text-[#ad7a41] ">Cookie</span> x Kozmos
+          </h1>
+          <h2 className=" text-white text-3xl font-bold mb-4 ">
+            <span className=" text-[#D9A65D] ">League Of Legends </span>
+            Turnuvası
+          </h2>
+          <div className="flex justify-center pt-5 space-x-4">
+            <a
+              href="/ContactForm"
+              className="bg-black hover:scale-110 duration-150 hover:bg-gray-800 shadow-md shadow-gray-700 text-white font-bold py-2 px-4 rounded-full"
+            >
+              Kayit Ol
+            </a>
+            <button className="bg-transparent border border-transparent hover:border-gray-500 text-white font-bold py-2 px-4 rounded-full">
+              Kurallari Oku
+            </button>
           </div>
         </div>
       </div>
